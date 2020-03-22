5a2b25ecc0760f40c72d4458603660ee8f21befe
Sören Söhngen <soeren.soehngen@gmail.com>
2020-03-22 17:03:49 +0100

    cleanup and improvents

--- client/src/components/Buttons/special/index.jsx
+++ client/src/components/Buttons/special/index.jsx
@@ -1,31 +1,25 @@
 import { hot } from 'react-hot-loader';
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { styled } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 
 const StyledButton = styled(Button)({
-    fontSize: '18px',
-    height: '48px',
-    paddingLeft: '16px',
-    paddingRight: '16px',
-    paddingTop: '12px',
-    paddingBottom: '12px'
-})
+  fontSize: '18px',
+  height: '48px',
+  paddingLeft: '16px',
+  paddingRight: '16px',
+  paddingTop: '12px',
+  paddingBottom: '12px',
+});
 
-export const SpecialButton = props => {
-    const primary = '#3f2c7a';
-    return (
-        <>
-            { props.selected ? (
-                <StyledButton color="primary" />
-            ) : (
-                <StyledButton variant="outlined" color="primary" />
-            ) }
-        </>
-    )
-}
+const SpecialButton = (props) => {
+  const { selected } = props;
+  return <>{selected ? <StyledButton color="primary" /> : <StyledButton variant="outlined" color="primary" />}</>;
+};
 
 SpecialButton.propTypes = {
-    selected: PropTypes.bool.isRequired
-}
+  selected: PropTypes.bool.isRequired,
+};
+
+export default hot(module)(SpecialButton);
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Step1 } from './Steps/step1';
+export { default as SpecialButton } from './Buttons/special';
