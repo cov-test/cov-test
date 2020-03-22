0a96eda2a4ae2df5f8c6d625b10c4bd2e78f6ef3
Andre Wisplinghoff <andre.wisplinghoff@gmail.com>
2020-03-22 13:13:10 +0100

    Add an error message if the JSON value is missing

--- server/src/backend.py
+++ server/src/backend.py
@@ -7,15 +7,19 @@ import classifier
 
 app = Flask(__name__)
 
+
 @app.route('/store_result', methods=['POST'])
 def store_result():
     data = request.get_json()
+    if data is None:
+        return jsonify({'success': False,
+                        'errorMessage': 'No JSON value provided or Content-Type of request not set to "application/json"'})
     test_data = answersparser.parse(data)
     doc_ref = db.collection("answers").document(test_data.userId)
     doc_ref.set(test_data)
     result = classifier.classify(test_data)
 
-    return jsonify({'result': result})
+    return jsonify({'success': True, 'result': result})
 
 
 if __name__ == '__main__':
