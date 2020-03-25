efdfa02bea9828d5be5d6daffe0d4700d560338f
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2020-03-25 20:44:22 +0100

    Add onSelection change (#25)
    * :sparkles: Add onSelection change
    
    function passed as prop
    
    * :fire: Delete test files
    
    * Object Destructuring
    
    * Object Destructuring

---
+++ client/src/components/Buttons/SpecialButtonGroup/index.jsx
@@ -0,0 +1,58 @@
+import { hot } from 'react-hot-loader';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '@material-ui/core';
+
+import SpecialButton from '../special/index';
+
+class SpecialButtonGroup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedButtons: new Set(),
+    };
+  }
+
+  static propTypes = {
+    buttons: PropTypes.arrayOf.isRequired,
+    onSelectionChange: PropTypes.func.isRequired,
+  };
+
+  handleButtonClick = (id) => {
+    const { onSelectionChange } = this.props;
+    const { selectedButtons } = this.state;
+    if (!selectedButtons.has(id)) {
+      this.setState({
+        selectedButtons: selectedButtons.add(id),
+      });
+    } else {
+      let tempSelected = new Set([...selectedButtons]);
+      tempSelected.delete(id);
+      this.setState({
+        selectedButtons: tempSelected,
+      });
+    }
+    onSelectionChange(selectedButtons);
+  };
+
+  render() {
+    const { buttons } = this.props;
+    const { selectedButtons } = this.state;
+    return (
+      <Box display="flex" flexDirection="row" flexWrap="wrap">
+        {buttons.map((button, index) => (
+          <SpecialButton
+            title={button}
+            onButtonClick={() => {
+              this.handleButtonClick(index);
+            }}
+            selected={selectedButtons.has(index)}
+          />
+        ))}
+      </Box>
+    );
+  }
+}
+
+export default hot(module)(SpecialButtonGroup);
