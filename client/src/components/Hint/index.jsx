b71266862d2ed4a1aec152d8ba6aaeb5ee94aebd
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2020-03-26 16:06:15 +0100

    Feature/hint component (#28)
    * Hint component
    
    * Add Hint Component
    
    Closes #17
    
    * Add Hint Component
    
    Closes #17
    
    * :fire: Remove unnecessary files
    
    * Update package.json

---
+++ client/src/components/Hint/index.jsx
@@ -0,0 +1,37 @@
+import { hot } from 'react-hot-loader';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Box, Typography } from '@material-ui/core';
+import { styled, makeStyles } from '@material-ui/core';
+
+import InfoIcon from '../Icons/info';
+
+const StyledIcon = styled(InfoIcon)({
+  marginRight: '16px',
+});
+
+const useStyles = makeStyles(() => ({
+  hintText: {
+    width: '232px',
+  },
+}));
+
+const Hint = (props) => {
+  const { text } = props;
+  const classes = useStyles();
+  return (
+    <Box display="flex" flexDirection="row" flexWrap="wrap">
+      <StyledIcon />
+      <Typography color="primary" className={classes.hintText}>
+        {text}
+      </Typography>
+    </Box>
+  );
+};
+
+Hint.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default hot(module)(Hint);
