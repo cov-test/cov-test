d21535a48f0e308b5eeac905035a9289cb4bea80
cgrozea <cgrozea@gmail.com>
2020-03-29 12:51:08 +0200

    Revert "test push"
    This reverts commit 4474d046af7363645e1612694a331ace1e9f7def.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,9 @@ import Container from '@material-ui/core/Container';
 import { styled, ThemeProvider } from '@material-ui/core/styles';
 import { Router } from '@reach/router';
 import theme from '../config/theme';
-import { Home, Questionnary, Question } from './views';
-import './App.css';
-import Hustenview from './Hustenview';
 
+import { Home, Questionnary, Question, Hustenview } from './views';
+import './App.css';
 
 const AppContainer = styled(Container)({
   padding: '0',
@@ -17,10 +16,10 @@ const App = () => (
     <AppContainer maxWidth="sm" className="app">
       <ThemeProvider theme={theme}>
         <Router primary={false}>
-          <Home path="/home" default />
+          <Home path="/" />
           <Questionnary path="/start" />
           <Question path="/question" />
-          {/* <Hustenview path="/" /> */}
+          <Hustenview path="/husten" default />
         </Router>
       </ThemeProvider>
     </AppContainer>
