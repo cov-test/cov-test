af858bf3fff63febf290b9137c184f08faa36005
Gunbit <soeren.soehngen@gmail.com>
2020-04-01 15:43:38 +0200

    Feature/remove neutrino (#34)
    * i18n WIP
    
    * remove_neutrino
    
    * json loader
    
    * [FEAT] parceljs
    
    * [FEAT] clean up
    
    Co-authored-by: Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>

--- client/src/views/index.jsx
+++ client/src/views/index.jsx
@@ -2,3 +2,4 @@
 export { default as Home } from './Home';
 export { default as Questionnary } from './Questionnary';
 export { default as Question } from './Question';
+export { default as TranslationTest } from './TranslationTest';
