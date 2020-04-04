c8b68b86aee43a5ac38b5d703ec26da026c80e43
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2020-04-04 15:23:08 +0200

    Feature/fieber component (#38)
    * NumperInput
    
    ref #18 and #20
    
    * Add UnitLabel Props
    
    * Fix for PR

--- client/src/views/index.jsx
+++ client/src/views/index.jsx
@@ -2,4 +2,3 @@
 export { default as Home } from './Home';
 export { default as Questionnary } from './Questionnary';
 export { default as Question } from './Question';
-export { default as TranslationTest } from './TranslationTest';
