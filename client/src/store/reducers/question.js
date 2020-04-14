24a6fb44fb665f48882ac2d80dc4a3e78beb8c8e
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2020-04-14 10:01:56 +0200

    Update reducer

--- client/src/store/reducers/question.js
+++ client/src/store/reducers/question.js
@@ -5,13 +5,20 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_ANSWER:
-      return [
-        ...state,
-        {
+      let findIndex = state.findIndex((obj) => obj.id == action.id);
+      let newAnswers = [...state];
+      if (findIndex !== -1) {
+        newAnswers[findIndex] = {
           id: action.id,
           answer: action.answer,
-        },
-      ];
+        };
+      } else {
+        newAnswers.push({
+          id: action.id,
+          answer: action.answer,
+        });
+      }
+      return newAnswers;
 
     default:
       return state;
